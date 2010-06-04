b61702004a5a7e1687ff3d98dfc294cd388de9ed
Marcin Wrochna <token@marcin.wrochnet>
2010-06-04 14:48:23 +0200

    dump bazy danych: www*.sql

--- tinymce/tmp/JSTrim/src/csharp/postbuild.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd ..\src\csharp
-rmdir /Q /S obj
--- tinymce/tmp/tiny_mce/plugins/latex/langs/fr.js
+++
@@ -1,9 +0,0 @@
-// FR lang variables
-
-tinyMCE.addToLang('',{
-insert_latex_desc : 'Insérer une formule latex',
-insert_latex_explain : 'Entrez juste le code mathmode de votre formule latex. Exemple : \\sqrt{a+b} <br /><a href="http://www.forkosh.dreamhost.com/source_mimetexmanual.html#examples" target="_blank">Plus d\'informations</a>',
-insert_latex_formula : 'Formule latex',
-insert_latex_preview1 : 'Visualisation',
-insert_latex_preview2 : 'Visualiser'
-});
