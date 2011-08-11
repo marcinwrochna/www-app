bea8d48bb53087fa397810c8a15724d78fd39efa
Marcin Wrochna <mwrochna+git@gmail.com>
2011-08-11 23:36:59 +0200

    Doxygen update

---
+++ cgi-bin/helloworldtest.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+print "Content-type:text/html\r\n\r\n";
+print '<html>';
+print '<head>';
+print '<title>Hello Word - First CGI Program</title>';
+print '</head>';
+print '<body>';
+print '<h2>perl www test</h2>';
+print '</body>';
+print '</html>';
+
+1;
