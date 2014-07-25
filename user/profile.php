01af5beff4910468cd58eda881f2e425a71ab84a
Jakub Sygnowski <sygi@google.com>
2014-07-25 11:30:16 +0200

    next change of the place

--- user/profile.php
+++ user/profile.php
@@ -345,11 +345,11 @@ function actionEditAdditionalInfo($uid = null)
 	$inputs['staybegintime']['options'] = $stayoptions;
 	$inputs['stayendtime']['options']   = $stayoptions;
 
-	$inputs['gatherplace']['options'] = array('warszawa'=>_('Warsaw PKP'),'olsztyn'=>_('Olsztyn PKP'),'wegorzewo'=>_('Węgorzewo - hostel'),'none'=>_('I\'ll arrive on my own.'));
+	$inputs['gatherplace']['options'] = array('warszawa'=>_('Warsaw PKP'),'wroclaw'=>_('Wrocław Główny'),'glogow'=>_('Głogów PKP'),'none'=>_('I\'ll arrive on my own.'));
 	$tshirtsizes = array('XS','S','M','L','XL','XXL');
 	$inputs['tshirtsize']['options'] = array_combine($tshirtsizes, $tshirtsizes);;
 	$inputs['isselfcatered']['text'] = _('on my own') .
-		'<small '. getTipJS(_('applies to Węgorzewo residents, for example')) .'>[?]</small>';
+		'<small '. getTipJS(_('applies to Głogów residents, for example')) .'>[?]</small>';
 
 
 	$form = new Form($inputs);
